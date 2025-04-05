bba3969e3babaa1ea42ec84f3f8515afbd64b0ae
Abril Ureña <abrilu@microsoft.com>
2025-04-05 01:42:18 -0600

    logic app

--- myfirstreflex_app/myfirstreflex_app.py
+++ myfirstreflex_app/myfirstreflex_app.py
@@ -1,18 +1,61 @@
 
 import reflex as rx
+from myfirstreflex_app.state import Tasks
 
-class Estado(rx.State):
-    x: int = 0
+def tarea_item(task):
+    return rx.hstack(
+        rx.checkbox(
+            is_checked = task["completed"],
+            on_change=lambda:Tasks.change_task_status(task["id"]),
+        ),
+        rx.text(
+            task["text"],
+            text_decoration="line-through" if task["completed"] else "none",
+            font_size="20px",
+            color="white",
+        ),
+        rx.spacer(),
+        rx.button(
+            "Delete",
+            on_click=lambda: Tasks.delete_task(task["id"]),
+            color_scheme="red",
+            color="white",
+            background_color="red.500",
+            width="100px",
+            height="40px",
+            border_buttom="1px solid white",
+        ),
+    )
 
-    def increment(self):
-        self.x += 1
-        return rx.vstack (
-           rx.text(f"Contador: {Estado.x}"),
-           rx.button("Incrementar", on_click=Estado.x)        
-           )
-
-def index() -> rx.Component:
-    return rx.text("Hello, Reflex!")
-
+def index():
+    return rx.center(
+        rx.vstack(
+            rx.heading("Todo List"),
+            rx.hstack(
+                rx.input(
+                    placeholder="Add a new task",
+                    value=Tasks.new_task,
+                    on_change=Tasks.set_new_task,
+                ),
+                rx.button(
+                    "add",
+                    on_click = Tasks.add_task,
+                    color_scheme="teal",
+                    color="white",
+                    background_color="teal.500",
+                ),
+            ),
+            rx.divider(),
+            rx.vstack(
+                rx.foreach(
+                    Tasks.tasks,
+                    tarea_item, 
+                )
+            ),
+            rx.text(
+                f"Total: {Tasks.tasks.id_task} tasks."
+            )
+        )
+    )
 app = rx.App()
 app.add_page(index)
--- myfirstreflex_app/state.py
+++ myfirstreflex_app/state.py
@@ -9,10 +9,24 @@ class Tasks(rx.State):
     def add_task(self):
         if self.new_task.strip() != "":
             # create a new unique id based on time
-            
+
             import time
             id_task = int(time.time() * 1000) 
 
             self.tasks.append({
-
+                "id": id_task,
+                "task": self.new_task,
+                "completed": False
             })
+
+            # clear the input field
+            self.new_task = ""
+    def change_task_status(self, id_task: int):
+        for i, task in enumerate(self.tasks):
+            if task["id"] == id_task:
+                self.tasks[i]["completed"] = not self.task[i]["completed"]
+                break
+    def delete_task(self, id_task: int):
+        self.tasks = [task for task in self.tasks if task["id"] != id_task]
+
+    
